fb5149ceb54fa250c2d75e1c62f3bbe4bcc393c7
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2019-10-14 23:37:10 +0000

    Fix hooks.py & add setup page

---
+++ custom_theme/custom_theme/doctype/custom_theme_setup/custom_theme_setup.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Zikri Marquel and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Custom Theme Setup', {
+	// refresh: function(frm) {
+
+	// }
+});
--- custom_theme/hooks.py
+++ custom_theme/hooks.py
@@ -15,11 +15,11 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/custom_theme/css/custom_theme.min.css"
+app_include_css = "/assets/css/custom_theme.min.css"
 # app_include_js = "/assets/custom_theme/js/custom_theme.js"
 
 # include js, css files in header of web template
-web_include_css = "/assets/custom_theme/css/custom_theme.min.css"
+web_include_css = "/assets/css/custom_theme.min.css"
 # web_include_js = "/assets/custom_theme/js/custom_theme.js"
 
 # include js in page
